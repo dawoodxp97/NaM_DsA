25e96be70268751d7d53d1bfc77e5234d1f9cd61
Dawood <dawoodmsam422@gmail.com>
2025-12-02 19:40:51 +0530

    refactor: add optimal shortform implementation for length of last word

--- STRINGS/lengthOfLastWord.js
+++ STRINGS/lengthOfLastWord.js
@@ -40,3 +40,14 @@ var lengthOfLastWord = function (s) {
 
     return res;
 };
+
+// Optimal shortform
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+    s = s.trim();
+    s = s.split(" ");
+    return s[s.length - 1].length
+};
